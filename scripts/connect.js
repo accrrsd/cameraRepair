d1f579eaed132be62a106da6ee97a4acd83f20d4
Daniil <kashel369@bk.ru>
2022-04-15 15:52:48 +0300

    after review

--- scripts/connect.js
+++ scripts/connect.js
@@ -1,20 +1,19 @@
 const connects = document.querySelectorAll('.connect')
 
-connects.forEach(item=>{
+connects.forEach((item) => {
   const input = item.querySelector('.connect__form')
-  input.addEventListener('input',inputLogic)
+  input.addEventListener('input', inputLogic)
 })
 
-function inputLogic(e){
+function inputLogic(e) {
   const currentInput = e.target
   const text = currentInput.parentNode.querySelector('.connect__btnText')
   const emoji = currentInput.parentNode.querySelector('.connect__emoji ')
-  if (currentInput.value){
-    text.textContent='Круто, спасибо за доверие!'
-    emoji.style.display='none'
-  }
-  else{
-    text.textContent='Отремонтируйте камеру'
-    emoji.style.display='inline'
+  if (currentInput.validity.valid) {
+    text.textContent = 'Круто, спасибо за доверие!'
+    emoji.style.display = 'none'
+  } else {
+    text.textContent = 'Отремонтируйте камеру'
+    emoji.style.display = 'inline'
   }
 }
